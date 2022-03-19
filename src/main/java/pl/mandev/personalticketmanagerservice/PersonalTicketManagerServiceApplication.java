6e9e255dae566f0a4b5f91f21513d3460f6fad70
GUCCI <kokoloko138@gmail.com>
2022-03-19 02:33:29 +0100

    Spring init

---
+++ src/main/java/pl/mandev/personalticketmanagerservice/PersonalTicketManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.mandev.personalticketmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTicketManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTicketManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mandev/personalticketmanagerservice/PersonalTicketManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mandev.personalticketmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTicketManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
